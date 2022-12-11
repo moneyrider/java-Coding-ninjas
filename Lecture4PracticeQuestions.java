602f1067b86eb11adafaefe8c4ac9b7cebd44313
Keshav <keshavgoswami885@gmail.com>
2022-12-11 23:26:01 +0530

    New Commit

---
+++ Lecture4PracticeQuestions.java
@@ -0,0 +1,24 @@
+package fundamentals;
+
+public class Lecture4PracticeQuestions {
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+//		byte b = 50;
+//		b = b * 50;
+		
+//		 double a = 6 / 4;
+//	        int b  = 6 / 4;
+//	        double c = a + b;
+//	        System.out.println(c);
+		
+		 double a = 55.5;
+	        int b = 55;
+	        a = a % 10;
+	        b = b % 10;
+	        System.out.println(a + " "  + b);
+
+	}
+
+}
---
+++ LogicalOperators.java
@@ -0,0 +1,25 @@
+package fundamentals;
+
+public class LogicalOperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10, b=20;
+//		System.out.println(true&&false);
+//		System.out.println(true||false);
+		
+		System.out.println(a>10 && b>10);
+		System.out.println(a>10 || b>10);
+		
+//		System.out.println(!true);
+		
+		System.out.println(!(a>b));
+		
+		// && operator work and returns true only when both condition are true.
+		// || operator work and returns true only when one condition is true.
+       //  ! operator work and returns opposite of conditions means true mean false 
+	  //      and false mean true .
+		
+		}
+
+}
